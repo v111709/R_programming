ef0cbff8ee7ffdf921970a0a72583b7446c32d4d
v111709 <35223109+v111709@users.noreply.github.com>
2018-03-11 11:29:16 -0400

    commit

---
+++ Module # 9 Visualization.R
@@ -0,0 +1,35 @@
+library(datasets)
+library(ggplot2)
+library(lattice)
+library(data.table)
+ 
+#--Nile
+#--structure(Nile)
+
+#--NileTimeseries < - Nile
+#--plot.ts(Nile)
+
+
+#----Read file from internet location using the data.table package
+nile <- fread("https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/datasets/Nile.csv")
+nile
+
+
+#---plot no Package
+
+plot.ts(Nile,col = "cyan", xlab = "Year", ylab = "Billions of cubic meters",pch=18, 
+     main = "Nile River Flow Measurements - No Package ")
+
+#---Lattice plot 
+nileplot <-xyplot(Nile, xlab = "Year", ylab = "Billions of cubic meters",pch=18, 
+                  main = "Nile River Flow Measurements - Lattice",col="cyan")
+print(nileplot)
+
+#---ggplot
+ ggplot(nile, aes(x = nile$time, y =nile$Nile )) +
+   geom_line() + labs(x="Year", y="Billions of cubic meters",color= "cyan",
+                      title = "Nile River Flow Measurements - ggplot") +  
+   geom_area(color = "cyan4", fill = "cyan4") + 
+  scale_color_manual(values = c("midnightblue", "navy")) +
+  scale_fill_manual(values = c("midnightblue", "navy")) + theme_light()
+
